7e9c8b05708ee35651231731fc45aa93a6081b4a
T-Kaito <take.off2357@gmail.com>
2024-08-19 17:58:17 +0900

    コンソールでイベントの作成

---
+++ script.js
@@ -0,0 +1,14 @@
+// window.alert('取得に成功')
+
+// document.getElementById('')
+
+// function getElement(){
+//     elements = document.querySelectorAll('.shop li');
+//     let len = elements.length;
+//     for (let i = 0; i < len; i++){
+//         console.log(elements.item(i).textContent);
+//     }
+// }
+
+let lists = document.getElementsByClassName('pull-down-list')
+console.log(lists)
