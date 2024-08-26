88ed7c00039d11148980617f427eef86c712624f
T-Kaito <take.off2357@gmail.com>
2024-08-27 00:37:20 +0900

    イベント発火　基礎編（JS10）

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ window.addEventListener('load', function() {
     const pullDownButton = document.getElementById("lists");
 
     pullDownButton.addEventListener('mouseover', () => {
+        pullDownButton.setAttribute("style", "background-color:blue;");
         console.log('乗った');
     });
-    　
-    pullDownButton.addEventListener('mouseout', () => {
-        console.log('離れた');
-    });
-    
+
     pullDownButton.addEventListener('click', () => {
+        pullDownButton.setAttribute("style", "background-color:green;");
         console.log('クリック');
     });
+    
+    pullDownButton.addEventListener('mouseout', () => {
+        pullDownButton.removeAttribute("style", "background-color:red;");
+        console.log('離れた');
+    });
+
 });
