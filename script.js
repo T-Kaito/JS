b6af8ce0cb8d44647a724463c151126a37e537c8
T-Kaito <take.off2357@gmail.com>
2024-08-27 02:43:34 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 window.addEventListener('load', function() {
 
-    const pullDownButton = document.getElementById("lists");
+    const pullDownButton = document.getElementById("lists")
+    const pullDownParents = document.getElementById("pull-down")
 
-    pullDownButton.addEventListener('mouseover', () => {
-        pullDownButton.setAttribute("style", "background-color:blue;");
-        console.log('乗った');
+    pullDownButton.addEventListener('mouseover', function() {
+        this.setAttribute("style", "background-color:blue;")
+        console.log('乗った')
     });
 
-    pullDownButton.addEventListener('click', () => {
-        pullDownButton.setAttribute("style", "background-color:green;");
-        console.log('クリック');
-    });
-    
-    pullDownButton.addEventListener('mouseout', () => {
-        pullDownButton.removeAttribute("style", "background-color:red;");
-        console.log('離れた');
+    pullDownButton.addEventListener('mouseout', function() {
+        this.removeAttribute("style", "background-color:red;")
+        console.log('離れた')
     });
 
+    pullDownButton.addEventListener('click', function() {
+        if (pullDownParents.getAttribute("style") == "display:block;"){
+            pullDownParents.removeAttribute("style", "display:block;");
+            console.log("非表示");
+        } else {
+            pullDownParents.setAttribute("style", "display:block;");
+            console.log("表示");
+        }
+        console.log('クリック')
+    });
 });
